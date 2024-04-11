590689ce1ff66d543d35434e236543f2d52c3940
deeprathod1 <rathoddeep182@gmail.com>
2024-04-11 12:01:36 +0530

    C-language

---
+++ MIHIRTAG.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+main(){
+int a =12;
+int b =6;
+
+clrscr();
+printf("addition:-%d\n",a+b);
+printf("subtraction:-%d\n",a-b);
+printf("multiplication:-%d\n",a*b);
+printf("division:-%d\n",a/b);
+printf("modulo:-%d",a%b);
+
+
+getch();
+
+}
